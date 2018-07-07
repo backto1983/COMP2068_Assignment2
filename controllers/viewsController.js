79ef5fee6b69b32218e27c03fb023f46293b63f6
backto1983 <200358165@student.georgianc.on.ca>
2018-07-07 12:56:13 -0400

    Added views (partials and index)

---
+++ controllers/viewsController.js
@@ -0,0 +1,21 @@
+/**
+ * File Name: viewController.js
+ * Author Name: Henrique Oliveira
+ * Website Name: Assignment 2 - NodeJS application using ExpressJS, MongoDB / Mongoose and EJS
+ * File Description: logical javaScript statements to control HTML views
+ */
+
+ // Each function below control one view (index, books, ...) 
+exports.homePage = (req, res, next) => {
+    res.render('index', { 
+      title: 'Assignment 2 - NodeJS application using ExpressJS, MongoDB / Mongoose and EJS',
+      name: 'ReadSpree',
+      message: 'Welcome to ReadSpree! Check out our collection of books and find only the best from many different genres.' });
+  };
+  
+  exports.books = (req, res, next) => {
+    const message = 'List of Books';
+    res.render('books', { 
+      title: 'Books',
+      message });       
+  };
