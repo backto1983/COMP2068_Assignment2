45d983868db24f6c38074b2c1dcc6a5413bf2e77
backto1983 <200358165@student.georgianc.on.ca>
2018-07-07 13:50:53 -0400

    Setup mongoose and dotenv in app.js

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Use mongoose to connect to mongodb
+require('dotenv').config({ path: 'variables.env' });
+const mongoose = require('mongoose'); 
+mongoose.connect(process.env.DATABASE)
+    .then(connection => {
+      console.log('Connected to MongoDB')
+    })
+    .catch(error => {
+      console.log(error.message)
+     });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
